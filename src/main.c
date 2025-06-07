4226cf64b25a2b47d8f1635ac4b0eaa2f05a8586
alitls <alitls8800@gmail.com>
2025-06-07 18:35:06 +0330

    implement git init command

--- src/main.c
+++ src/main.c
@@ -2,8 +2,46 @@
 // Created by alireza on 5/29/25.
 //
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Implementing part of git")
+bool init();
+
+int main(int argc, char *argv[]) {
+    printf("Implementing part of git\n");
+
+    printf("argc: %d\n", argc);
+    if (argc < 2) {
+        printf("Usage ./program.sh <command> [<args>]\n");
+        return 1;
+    }
+    const char *command = argv[1];
+    printf("arg: %s\n", command);
+
+    if (strcmp(command, "init") == 0) {
+        if (init()) {
+            printf("Failed to create directories\n");
+        }
+        FILE *headFile = fopen(".git/HEAD", "w");
+        if (headFile == NULL) {
+            printf("Failed to create .git/HEAD file\n");
+            return 1;
+        }
+        fprintf(headFile, "ref: refs/heads/main\n");
+        fclose(headFile);
+
+        printf("Init git directory\n");
+    } else {
+        printf("Unknown command %s\n", command);
+        return 1;
+    }
+
     return 0;
+}
+
+bool init() {
+    return mkdir(".git", 0755) == -1 ||
+           mkdir(".git/objects", 0755) == -1 ||
+           mkdir(".git/refs", 0755) == -1;
 }
