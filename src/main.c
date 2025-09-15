d8566edb0574eab952b7dcea085c25b8ea1656db
alitls <alitls.8800@gmail.com>
2025-09-15 19:37:40 +0330

    add git hash-object

--- src/main.c
+++ src/main.c
@@ -4,12 +4,59 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <zlib.h>
+#include <openssl/sha.h>
 
 int myGit(int argc, char **pString);
+
+int zlib_cd();
+
+void hash();
 
 int main(int argc, char *argv[]) {
     return myGit(argc, argv);
 }
+
+void hash() {
+    unsigned char hash[SHA_DIGEST_LENGTH];
+
+    const char *msg = "Hello world";
+    SHA1((unsigned char *) msg, strlen(msg), hash);
+
+    printf("%s\n", hash);
+}
+
+int zlib_cd() {
+    const char *input = "Hello, world! This is a zlib compression test.";
+    uLong input_size = strlen(input) + 1; // include '\0'
+
+    // Buffers for compressed and decompressed data
+    unsigned char compressed[1024];
+    unsigned char decompressed[1024];
+
+    uLong compressed_size = sizeof(compressed);
+    uLong decompressed_size = sizeof(decompressed);
+
+    // Compress the input string
+    if (compress(compressed, &compressed_size, (const Bytef *) input, input_size) != Z_OK) {
+        printf("Compression failed!\n");
+        return 1;
+    }
+
+    printf("Original size: %lu bytes\n", input_size);
+    printf("Compressed size: %lu bytes\n", compressed_size);
+
+    // Decompress back into decompressed buffer
+    if (uncompress(decompressed, &decompressed_size, compressed, compressed_size) != Z_OK) {
+        printf("Decompression failed!\n");
+        return 1;
+    }
+
+    printf("Decompressed size: %lu bytes\n", decompressed_size);
+    printf("Decompressed string: %s\n", decompressed);
+
+    return 0;
+}
+
 
 int myGit(int argc, char *argv[]) {
 
@@ -125,6 +172,103 @@ int myGit(int argc, char *argv[]) {
         //(total decompressed size minus the header length)
         //content - decompressed = 8 (because "blob 12\0" is 8 bytes long)
         fwrite(content, 1, stream.total_out - (content - decompressed), stdout);
+
+    } else if (strcmp(command, "hash-object") == 0) {
+        // 1 - check command is ok
+        if (argc != 4 || strcmp(argv[2], "-w") != 0) {
+            fprintf(stderr, "Usage ./your_program.sh hash-object -w <object>\n");
+            return 1;
+        }
+
+        char *file_path = argv[3];
+        printf("%s\n", file_path);
+
+        //just a new file ready to create object from it.
+        FILE *fp = fopen(file_path, "rb");
+        if (!fp) {
+            fprintf(stderr, "Failed to open file\n");
+            return 78;
+        }
+
+        fseek(fp, 0, SEEK_END);
+        long file_size = ftell(fp);
+        rewind(fp); //back to beginning
+
+        //content of file
+        char *content = malloc(file_size);
+        if (content == NULL) {
+            fprintf(stderr, "Failed to get memory for content: %s\n", strerror(errno));
+        }
+        fread(content, 1, file_size, fp);
+        fclose(fp);
+
+        //format of compressed file blob <size>\0<content>
+        //header of compressed file: blob <size>\0
+        char header[64];
+        int header_len = snprintf(header, sizeof(header), "blob %ld", file_size);
+
+        //full_len is header + \0 + file_size
+        int full_len = header_len + 1 + file_size;
+        unsigned char *buffer = malloc(full_len);
+        //copy header to buffer
+        memcpy(buffer, header, header_len);
+        //adding \0 char
+        buffer[header_len] = '\0';
+        //copy content to end of buffer
+        memcpy(buffer + header_len + 1, content, file_size);
+
+        //hash with openssl/sha.h
+        //sha-1 digest is always 20 byte
+        unsigned char sha1_hash[SHA_DIGEST_LENGTH];
+        SHA1(buffer, full_len, sha1_hash);
+
+        //Each byte will be shown as two hexadecimal characters (00–ff).
+        //20 bytes × 2 chars = 40 characters plus 1 byte for the string terminator '\0'.
+        char sha1_hex[41];
+        //convert each byte to two hex digit
+        for (int i = 0; i < SHA_DIGEST_LENGTH; i++) {
+            sprintf(sha1_hex + i * 2, "%02x", sha1_hash[i]);
+        }
+        sha1_hex[40] = '\0';
+
+        //Returns the maximum possible size the compressed data could take
+        uLong compressed_size = compressBound(full_len);
+        //holds “biggest size we might need
+        Bytef *compressed_data = malloc(compressed_size);
+        if (compressed_data == NULL) {
+            fprintf(stderr, "Failed to get memory for compressed_data: %s\n", strerror(errno));
+        }
+
+        if (compress(compressed_data, &compressed_size, (Bytef *) buffer, full_len) != Z_OK) {
+            fprintf(stderr, "Compression failed!");
+            exit(99);
+        }
+
+        char dir[64];
+        snprintf(dir, sizeof(dir), ".git/objects/%.2s", sha1_hex);
+        if (mkdir(dir, 0755) == -1) {
+            fprintf(stderr, "Failed to create directory: %s\n", strerror(errno));
+        }
+
+        char file[128];
+        sprintf(file, ".git/objects/%.2s/%s", sha1_hex, sha1_hex + 2);
+        printf("file: %s\n", file);
+
+        FILE *ffp = fopen(file, "wb");
+        if (!ffp) {
+            fprintf(stderr, "Failed to open file for writing binary: %s\n", strerror(errno));
+            return 79;
+        }
+
+        fwrite(compressed_data, 1, compressed_size, ffp);
+
+        fclose(ffp);
+
+        printf("%s\n", sha1_hex);
+
+        free(buffer);
+        free(compressed_data);
+        free(content);
 
     } else {
         fprintf(stderr, "Unknown command %s\n", command);
